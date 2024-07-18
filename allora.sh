12e1924bddedef565d4eba46e82fccc2387b0a2b
cryptoneth <149672650+cryptoneth@users.noreply.github.com>
2024-07-19 02:46:58 +0330

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -331,5 +331,5 @@ echo
 echo -e "${BOLD}${DARK_YELLOW}Checking running Docker containers...${RESET}"
 docker ps
 echo
-echo "${BOLD}${DARK_YELLOW}Follow me on Twitter @ZunXBT for one click node run guides !!${RESET}"
+echo "${BOLD}${DARK_YELLOW}Crypton !!${RESET}"
 echo
